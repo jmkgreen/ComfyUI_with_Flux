58276ad7f9d429df6c152e37f24fbdf3cf890ffc
James Green <james.mk.green@gmail.com>
2025-09-25 10:17:36 +0100

    Portability; fs prefix

--- comfyui-without-flux/scripts/download_wan2.1.sh
+++ comfyui-without-flux/scripts/download_wan2.1.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Downloading umt5_xxl_fp8_e4m3fn_scaled.safetensors text encoders"
-cd /ComfyUI/models/text_encoders/
+cd /workspace/ComfyUI/models/text_encoders/
 
 file="umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors?download=true"
@@ -15,7 +15,7 @@ fi
 
 
 echo "Downloading wan_2.1_vae.safetensors"
-cd /ComfyUI/models/vae/
+cd /workspace/ComfyUI/models/vae/
 
 file="wan_2.1_vae.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors?download=true"
@@ -28,7 +28,7 @@ else
 fi
 
 echo "Downloading wan2.1_i2v_720p_14B_bf16.safetensors"
-cd /ComfyUI/models/diffusion_models/
+cd /workspace/ComfyUI/models/diffusion_models/
 
 file="wan2.1_i2v_720p_14B_bf16.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_720p_14B_bf16.safetensors?download=true"
@@ -41,7 +41,7 @@ else
 fi
 
 echo "Downloading clip_vision_h.safetensors"
-cd /ComfyUI/models/clip_vision/
+cd /workspace/ComfyUI/models/clip_vision/
 
 file="clip_vision_h.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors?download=true"
@@ -54,7 +54,7 @@ else
 fi
 
 echo "Downloading wan2.1_t2v_14B_bf16.safetensors"
-cd /ComfyUI/models/diffusion_models/
+cd /workspace/ComfyUI/models/diffusion_models/
 
 file="wan2.1_t2v_14B_bf16.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_t2v_14B_bf16.safetensors?download=true"
