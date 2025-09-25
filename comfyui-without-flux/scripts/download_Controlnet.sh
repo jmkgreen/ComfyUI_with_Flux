d06ad249e1675d847a885397d77901409972fe0d
James Green <james.mk.green@gmail.com>
2025-09-25 09:42:59 +0100

    Be more portable; use HF for 4x-UltraSharp

--- comfyui-without-flux/scripts/download_Controlnet.sh
+++ comfyui-without-flux/scripts/download_Controlnet.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Downloading Shakker-labs Controlnet"
-cd /ComfyUI/models/controlnet
+cd /workspace/ComfyUI/models/controlnet
 
 file="diffusion_pytorch_model.safetensors"
 url="https://huggingface.co/Shakker-Labs/FLUX.1-dev-ControlNet-Union-Pro/resolve/main/diffusion_pytorch_model.safetensors?download=true"
@@ -14,7 +14,7 @@ else
 fi
 
 echo "Downloading xinsir controlnet"
-cd /ComfyUI/models/controlnet
+cd /workspace/ComfyUI/models/controlnet
 
 file="xinsir_controlnet_union-sdxl-1-promax.safetensors"
 url="https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model_promax.safetensors?download=true"
@@ -27,9 +27,9 @@ else
 fi
 
 echo "Downloading 4x-UltraSharp.pth"
-cd /ComfyUI/models/upscale_models
+cd /workspace/ComfyUI/models/upscale_models
 file="4x-UltraSharp.pth"
-url="https://civitai.com/api/download/models/125843?type=Model&format=PickleTensor"
+url="https://huggingface.co/lokCX/4x-Ultrasharp/resolve/main/4x-UltraSharp.pth?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
