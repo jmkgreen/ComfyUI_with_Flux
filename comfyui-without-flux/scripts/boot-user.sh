9eb84893b0e36faf7e2d4cfb539c102ed28e1c63
James Green <james.mk.green@gmail.com>
2025-09-29 10:50:25 +0100

    FIx paths

--- comfyui-without-flux/scripts/boot-user.sh
+++ comfyui-without-flux/scripts/boot-user.sh
@@ -4,11 +4,11 @@
 # Things like installing additional custom nodes, or downloading models can be done here.
 
 # Update the included workflows
-bash /update_Workflows.sh
+/scripts/update_Workflows.sh
 
 # Disable Mixlab nodes because they take a long time load and are no longer needed in any of the included workflows.
 # But can be enabled if needed by commenting out the following line.
-bash /disable_mixlab.sh
+/scripts/disable_mixlab.sh
 
 # Launch the UI
 python3 /workspace/ComfyUI/main.py --listen
