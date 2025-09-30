e12b919210b0b78c00ce8b281bed9f00c50fa372
James Green <james.mk.green@gmail.com>
2025-09-30 12:27:48 +0100

    More robust ComfyUI install

--- comfyui-without-flux/scripts/install-comfyui.sh
+++ comfyui-without-flux/scripts/install-comfyui.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 echo "Installing ComfyUI to /workspace/ComfyUI"
+source /workspace/venv/bin/activate
 cd /workspace && \
     git clone https://github.com/comfyanonymous/ComfyUI.git && \
     cd ComfyUI && \
-    pip3 install -r requirements.txt
+    pip3 install -r requirements.txt &&
+    echo "ComfyUI installed to /workspace/ComfyUI"
+
 
 echo "Installing custom nodes"
-cd custom_nodes && \
+cd /workspace/ComfyUI/custom_nodes && \
     git clone https://github.com/ltdrdata/ComfyUI-Manager.git && \
     git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git && \
     cd /workspace/ComfyUI && \
     mkdir pysssss-workflows
-echo "ComfyUI installed to /workspace/ComfyUI"
+echo "Custom nodes installed"
