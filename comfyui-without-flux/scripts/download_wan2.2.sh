b69e1f7384d7910f16c5dcf799bd963bcd93005e
James Green <james.mk.green@gmail.com>
2025-09-30 11:52:41 +0100

    wan2.2: More models

--- comfyui-without-flux/scripts/download_wan2.2.sh
+++ comfyui-without-flux/scripts/download_wan2.2.sh
@@ -14,10 +14,10 @@ else
 fi
 
 
-echo "Downloading wan_2.2_vae.safetensors"
+file="wan_2.2_vae.safetensors"
+echo "Downloading ${file}"
 cd /workspace/ComfyUI/models/vae/
 
-file="wan_2.2_vae.safetensors"
 url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan2.2_vae.safetensors?download=true"
 
 if [ -f "$file" ]; then
@@ -27,38 +27,11 @@ else
     wget -O $file $url --progress=bar:force:noscroll
 fi
 
-echo "Downloading wan2.2_i2v_high_noise_14B_fp16.safetensors"
-cd /workspace/ComfyUI/models/diffusion_models/
-
-file="wan2.2_i2v_high_noise_14B_fp16.safetensors"
-url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp16.safetensors?download=true"
-
-if [ -f "$file" ]; then
-    echo "$file already exists."
-else
-    echo "Downloading $file"
-    wget -O $file $url --progress=bar:force:noscroll
-fi
-
-echo "Downloading wan2.2_i2v_low_noise_14B_fp16.safetensors"
-cd /workspace/ComfyUI/models/diffusion_models/
-
-file="wan2.2_i2v_low_noise_14B_fp16.safetensors"
-url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors?download=true"
-
-if [ -f "$file" ]; then
-    echo "$file already exists."
-else
-    echo "Downloading $file"
-    wget -O $file $url --progress=bar:force:noscroll
-fi
-
-
-echo "Downloading clip_vision_h.safetensors"
+file="clip_vision_h.safetensors"
+echo "Downloading ${file}"
 cd /workspace/ComfyUI/models/clip_vision/
 
-file="clip_vision_h.safetensors"
-url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors?download=true"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/clip_vision/${file}?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
@@ -67,11 +40,11 @@ else
     wget -O $file $url --progress=bar:force:noscroll
 fi
 
-echo "Downloading wan2.2_t2v_high_noise_14B_fp16.safetensors"
+file="wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors"
+echo "Downloading ${file}"
 cd /workspace/ComfyUI/models/diffusion_models/
 
-file="wan2.2_t2v_high_noise_14B_fp16.safetensors"
-url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp16.safetensors?download=true"
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/${file}?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
@@ -79,12 +52,78 @@ else
     echo "Downloading $file"
     wget -O $file $url --progress=bar:force:noscroll
 fi
-
-echo "Downloading wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
-cd /workspace/ComfyUI/models/diffusion_models/
 
 file="wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
-url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors?download=true"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/diffusion_models/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/${file}?download=true"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+file="wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/diffusion_models/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/${file}?download=true"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+file="wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/diffusion_models/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/${file}?download=true"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+file="wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/loras/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/${file}?download=true"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+file="wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/diffusion_models/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/${file}?download=true"
+
+if [ -f "$file" ]; then
+    echo "$file already exists."
+else
+    echo "Downloading $file"
+    wget -O $file $url --progress=bar:force:noscroll
+fi
+
+
+file="wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors"
+echo "Downloading ${file}"
+cd /workspace/ComfyUI/models/loras/
+
+url="https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/${file}?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
