e79ddb7731d7a23f57d48d51095f03b526ed724b
James Green <james.mk.green@gmail.com>
2025-10-03 12:26:03 +0100

    qwen: use lighter 2509 diffusion and 4-step lightning,, used in official workflow

--- comfyui-without-flux/scripts/download_qwen-bf16.sh
+++ comfyui-without-flux/scripts/download_qwen-bf16.sh
@@ -27,20 +27,20 @@ else
     wget -O $file $url --progress=bar:force:noscroll
 fi
 
-file="qwen_image_fp8_e4m3fn.safetensors"
-echo "Downloading ${file}"
-cd /workspace/ComfyUI/models/diffusion_models/
+# file="qwen_image_fp8_e4m3fn.safetensors"
+# echo "Downloading ${file}"
+# cd /workspace/ComfyUI/models/diffusion_models/
 
-url="https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/diffusion_models/${file}?download=true"
+# url="https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/diffusion_models/${file}?download=true"
 
-if [ -f "$file" ]; then
-    echo "$file already exists."
-else
-    echo "Downloading $file"
-    wget -O $file $url --progress=bar:force:noscroll
-fi
+# if [ -f "$file" ]; then
+#     echo "$file already exists."
+# else
+#     echo "Downloading $file"
+#     wget -O $file $url --progress=bar:force:noscroll
+# fi
 
-file="qwen_image_edit_2509_bf16.safetensors"
+file="qwen_image_edit_2509_fp8_e4m3fn.safetensors"
 echo "Downloading ${file}"
 cd /workspace/ComfyUI/models/diffusion_models/
 
@@ -53,7 +53,8 @@ else
     wget -O $file $url --progress=bar:force:noscroll
 fi
 
-file="Qwen-Image-Edit-Lightning-8steps-V1.0.safetensors"
+
+file="Qwen-Image-Edit-Lightning-4steps-V1.0.safetensors"
 echo "Downloading ${file}"
 cd /workspace/ComfyUI/models/loras/
 
