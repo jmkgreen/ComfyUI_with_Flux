ff62d9ae939489fa89cdedb07be886dff244fa41
James Green <james.mk.green@gmail.com>
2026-01-27 10:39:04 +0000

    Fix missing 'h'

--- comfyui-without-flux/scripts/install-comfyui.sh
+++ comfyui-without-flux/scripts/install-comfyui.sh
@@ -14,7 +14,7 @@ cd /workspace/ComfyUI/custom_nodes && \
     git clone https://github.com/MadiatorLabs/ComfyUI-RunpodDirect.git && \
     git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack && \
     git clone https://github.com/rgthree/rgthree-comfy.git && \
-    git clone ttps://github.com/yolain/ComfyUI-Easy-Use && \
+    git clone https://github.com/yolain/ComfyUI-Easy-Use && \
     git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git && \
     git clone https://github.com/ShammiG/ComfyUI-Simple_Readable_Metadata-SG && \
     git clone https://github.com/kijai/ComfyUI-KJNodes && \
